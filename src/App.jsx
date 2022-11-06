1c7cc707099fc0e796f4f0e4efd37782db063ef8
GoharH <hovsepyanga@gmail.com>
2022-11-06 22:58:16 +0300

    created full page

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/header";
+import Main from "./components/main";
+import Stuff from "./components/staff-section";
+import Footer from "./components/footer";
+
+function App() {
+  return (<>
+
+    <Header />
+    <Main />
+    <Stuff />
+    <Footer />
+
+  </>
+  );
+}
+
+export default App;
