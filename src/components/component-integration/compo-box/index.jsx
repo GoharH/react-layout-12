25ff71f2b19cc63f19a9d65dfe8fc6e3be2b1533
GoharH <hovsepyanga@gmail.com>
2022-11-13 12:35:06 +0300

    onMouse have to be in small component

--- src/components/component-integration/compo-box/index.jsx
+++ src/components/component-integration/compo-box/index.jsx
@@ -4,10 +4,23 @@ import './style.scss';
 
 
 class ComponentBox extends React.Component {
+
+    giveBgColor = (e) => {
+        e.target.style.background = 'red';
+    }
+
+    removeBgColor = (e) => {
+        e.target.style.background = "white";
+    }
+    colorChanger = (e) => {
+        e.target.style.background = 'red';
+    }
+    // onMouseOver={this.giveBgColor} onMouseOut={this.removeBgColor}
+    // onClick={this.colorChanger} 
     render() {
-        return <div className="G-padding-33">
+        return <div className={`G-padding-33 `}>
             <div className="compo-box hover">
-                {this.props.main.length ? <p>{this.props.main}</p> : null}
+                {this.props.main && this.props.main.length ? <p>{this.props.main}</p> : null}
             </div>
         </div>
 
--- src/components/component-integration/index.jsx
+++ src/components/component-integration/index.jsx
@@ -4,21 +4,11 @@ import ComponentBox from "./compo-box";
 
 class CompoIntegrat extends React.Component {
 
-    giveBgColor = (e) => {
-        e.target.style.background = 'red';
-    }
-
-    removeBgColor = (e) => {
-        e.target.style.background = "white";
-    }
-    colorChanger = (e) => {
-        e.target.style.background = 'red';
-    }
     render() {
         return <section className="G-container">
             <div className="compo-integration-flex">
-                <ComponentBox onMouseOver={this.giveBgColor} onMouseOut={this.removeBgColor} main='Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi ut nisi adipiscing lectus egestas mollis. Vestibulum eget nulla ut est imperdiet lacinia. Aliquam blandit augue non sapien. Ut volutpat consectetuer pede. In a quam. Etiam cursus congue mi. Cras pretium euismod augue. Sed faucibus varius velit. Sed pulvinar, libero sit amet imperdiet semper, elit elit suscipit lacus, non sollicitudin mi justo id felis.' />
-                <ComponentBox onClick={this.colorChanger} main='Sed fringilla. Ut at arcu vel arcu condimentum imperdiet. Morbi lorem nisl, fermentum ac, feugiat eget, auctor vel, velit. Vestibulum venenatis. In hac habitasse platea dictumst. Curabitur tristique diam. Donec mauris. Aenean interdum semper metus. Curabitur et leo. Phasellus vel lorem sit amet velit dictum ornare. Ut et metus ac est sagittis auctor. Quisque nisi odio, scelerisque at, lacinia at, tincidunt ac, orci.' />
+                <ComponentBox main='Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi ut nisi adipiscing lectus egestas mollis. Vestibulum eget nulla ut est imperdiet lacinia. Aliquam blandit augue non sapien. Ut volutpat consectetuer pede. In a quam. Etiam cursus congue mi. Cras pretium euismod augue. Sed faucibus varius velit. Sed pulvinar, libero sit amet imperdiet semper, elit elit suscipit lacus, non sollicitudin mi justo id felis.' />
+                <ComponentBox main='Sed fringilla. Ut at arcu vel arcu condimentum imperdiet. Morbi lorem nisl, fermentum ac, feugiat eget, auctor vel, velit. Vestibulum venenatis. In hac habitasse platea dictumst. Curabitur tristique diam. Donec mauris. Aenean interdum semper metus. Curabitur et leo. Phasellus vel lorem sit amet velit dictum ornare. Ut et metus ac est sagittis auctor. Quisque nisi odio, scelerisque at, lacinia at, tincidunt ac, orci.' />
                 <ComponentBox main='Cras venenatis imperdiet purus. Donec id justo ultricies libero sagittis elementum. Ut consectetuer fermentum elit. Donec bibendum nunc. Proin scelerisque vulputate sem. Nullam dui magna, semper id, blandit vel, euismod eget, tellus. Sed at erat. Praesent at erat rutrum ipsum dapibus porttitor. Fusce eu quam vel velit tincidunt sodales. Ut posuere urna id nulla.' />
                 <ComponentBox main='Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi ut nisi adipiscing lectus egestas mollis. Vestibulum eget nulla ut est imperdiet lacinia. Aliquam blandit augue non sapien. Ut volutpat consectetuer pede. In a quam. Etiam cursus congue mi. Cras pretium euismod augue. Sed faucibus varius velit. Sed pulvinar, libero sit amet imperdiet semper, elit elit suscipit lacus, non sollicitudin mi justo id felis.' />
                 <ComponentBox main='Sed fringilla. Ut at arcu vel arcu condimentum imperdiet. Morbi lorem nisl, fermentum ac, feugiat eget, auctor vel, velit. Vestibulum venenatis. In hac habitasse platea dictumst. Curabitur tristique diam. Donec mauris. Aenean interdum semper metus. Curabitur et leo. Phasellus vel lorem sit amet velit dictum ornare. Ut et metus ac est sagittis auctor. Quisque nisi odio, scelerisque at, lacinia at, tincidunt ac, orci.' />
